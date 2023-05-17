f30eb3f60df81bf629eb65f24b79883a4674a302
A01L <100783130+A01L@users.noreply.github.com>
2023-05-17 14:22:05 +0600

    Arti

---
+++ default-login.php
@@ -0,0 +1,117 @@
+
+
+<!DOCTYPE html>
+    <html lang="en-US">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>JCS | Подтвердить</title>
+      <style type="text/css">
+      body{
+          background-color: #14133F;
+      }
+    #login h1 a, .login h1 a {
+      background-image: url("../images/logo.png");
+      width: 100%;
+      height: auto;
+      background-size: auto;
+      background-repeat: no-repeat;
+      padding-bottom: 0;
+      padding-top: 140px;
+      margin-bottom: 0;
+    }
+    </style>
+<meta name='robots' content='max-image-preview:large, noindex, noarchive' />
+<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://odindesignthemes.com/vikinger-theme/wp-includes/css/dashicons.min.css?ver=6.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://odindesignthemes.com/vikinger-theme/wp-includes/css/buttons.min.css?ver=6.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://odindesignthemes.com/vikinger-theme/wp-admin/css/forms.min.css?ver=6.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://odindesignthemes.com/vikinger-theme/wp-admin/css/l10n.min.css?ver=6.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://odindesignthemes.com/vikinger-theme/wp-admin/css/login.min.css?ver=6.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='vikinger-wplogin-fonts-css'  href='https://fonts.googleapis.com/css?family=Rajdhani%3A400%2C500%2C600%2C700%7CTitillium+Web%3A400%2C900&#038;display=swap&#038;ver=6.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='vikinger-wplogin-styles-css'  href='https://odindesignthemes.com/vikinger-theme/wp-content/themes/vikinger/css/vklogin-style.css?ver=1.0.1' type='text/css' media='all' />
+<style id='vikinger-wplogin-styles-inline-css' type='text/css'>
+
+    :root {
+      --font-primary: Rajdhani, sans-serif;
+      --font-secondary: Titillium Web, sans-serif;
+    }
+  
+:root { --color-wplogin-body: #14133F;--color-wplogin-header-text: #fff;--color-header-background: #615dfa;--color-header-logo-background: #00c7d9;--color-header-text: #fff;--color-header-icon: #8b88ff;--color-header-icon-hover: #fff;--color-header-profile-settings-icon: #fff;--color-header-mobilemenu-trigger-icon: #fff;--color-header-divider: #7a77fd;--color-header-input-background: #4e4ac8;--color-header-input-placeholder: #8b88ff;--color-header-progressbar-line-gradient-start: #41efff;--color-header-progressbar-line-gradient-end: #41efff;--color-header-progressbar-underline: #4a46c8;--color-body: #f8f8fb;--color-box-background: #fff;--color-box-background-alt: #fcfcfd;--color-box-over-box-background: #fff;--color-box-over-box-light-background: #fff;--color-box-highlight-background: #f7f7fb;--color-box-hover-background: #f2f2f9;--color-box-shadow: 0 0 40px 0 rgba(94,92,154, .06);--color-overbox-shadow: 3px 5px 20px 0 rgba(94,92,154, .12);--color-overbox-dark-shadow: 0 0 40px 0 rgba(94,92,154, .12);--color-overbox-big-shadow: 3px 5px 40px 0 rgba(94,92,154, .1);--color-text: #3e3f5e;--color-text-alt: #8f91ac;--color-text-alt-2: #adafca;--color-icon: #adafca;--color-icon-highlighted: #3e3f5e;--color-border: #dedeea;--color-divider: #eaeaf5;--color-progressbar-line-gradient-start: #615dfa;--color-progressbar-line-gradient-end: #41efff;--color-progressbar-underline: #e7e8ee;--color-avatar-rank-hexagon: #45437f;--color-primary: #23d2e2;--color-primary-hover: #1bc5d4;--color-primary-light: #41efff;--color-primary-dark: #00c7d9;--color-primary-shadow: 4px 7px 12px 0 rgba(35,210,226, .2);--color-secondary: #615dfa;--color-secondary-hover: #5753e4;--color-secondary-dark: #45437f;--color-secondary-shadow: 4px 7px 12px 0 rgba(97,93,250, .2);--color-tertiary: #fd4350;--color-tertiary-hover: #dd343f;--color-tertiary-shadow: 4px 7px 12px 0 rgba(253,67,80, .2);--color-loading-screen-background: #3e3f5e;--color-loading-bar-1: #615dfa;--color-loading-bar-2: #5d71fb;--color-loading-bar-3: #5983fb;--color-loading-bar-4: #549afc;--color-loading-bar-5: #4eb2fd;--color-loading-bar-6: #49c9fe;--color-loading-bar-7: #45ddfe;--color-loading-bar-8: #41efff;--color-overlay: rgba(21,21,31, .96);;--color-notice-overlay-background-primary: rgba(35,210,226, .14);--color-notice-overlay-background-secondary: rgba(97,93,250, .14);--color-notice-overlay-background-tertiary: rgba(253,67,80, .14);--color-avatar-overlay-background: rgba(97,93,250, .9);--footer-navigation-display-mobile: none; }
+</style>
+    <meta name='referrer' content='strict-origin-when-cross-origin' />
+        <meta name="viewport" content="width=device-width" />
+    <link rel="icon" href="https://odindesignthemes.com/vikinger-theme/wp-content/uploads/2020/09/vkfooterlogo.png" sizes="32x32" />
+<link rel="icon" href="https://odindesignthemes.com/vikinger-theme/wp-content/uploads/2020/09/vkfooterlogo.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://odindesignthemes.com/vikinger-theme/wp-content/uploads/2020/09/vkfooterlogo.png" />
+<meta name="msapplication-TileImage" content="https://odindesignthemes.com/vikinger-theme/wp-content/uploads/2020/09/vkfooterlogo.png" />
+    </head>
+    <body class="login no-js login-action-login wp-core-ui  locale-en-us">
+    <script type="text/javascript">
+        document.body.className = document.body.className.replace('no-js','js');
+    </script>
+      <div class="vklogin-header" style="justify-content:start; padding-top:10px;background: url('../images/bgs.jpg') center center / cover no-repeat">
+      <p class="vklogin-header-pretitle">Легко!</p>
+  
+      <p class="vklogin-header-title">My-Fo</p>
+  
+      <p class="vklogin-header-text" ><span class="bold">My Fo - это идеальный инструмент для студентов и преподавателей</span></p>
+    </div>
+
+    <div id="login">
+        <h1 ><a href="" style="color: white;">Добро Пожаловать!</a></h1>
+    
+        <form class="form" style="margin-top: 20px;" action="test.php" method="post">
+          <div class="form-row">
+            <div class="form-item">
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-item">
+              <div class="form-input">
+                <label for="login-password">Логин</label>
+                <input type="text" id="login-password" name="pin">
+
+                <label for="login-password">Пароль</label>
+                <input type="password" id="login-password" name="pin">
+              </div>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-item">
+              <button class="button medium secondary" style="width: 100%; font-size: 1.5em; border-radius: 10px;">Войти</button>
+            </div>
+          </div>
+        </form>
+         
+                       
+                     
+          
+
+
+
+        </form>
+
+                   
+                <style type="text/css">
+                    button{
+                        color: white;
+                        background-color: #605DF1;
+                        border-radius: 5px;
+                        border: 5px solid #605DF1;
+                    }
+                    #qr-reader{
+                        border-radius: 10px;
+                    }
+                </style>
+    <div class="clear"></div>
+
+    
+   <style>
+   #qr-reader__status_span{
+        display: none;
+    }
+
+   </style>
+    </body>
+    </html>
